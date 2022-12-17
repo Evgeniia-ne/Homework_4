31d1a0db53b521f894bba8faae1d3d4f1b32415f
Asus <koncarik@mail.ru>
2022-12-17 23:11:58 +0500

    Домашнее задание

---
+++ Zadacha_27/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.Clear();
+int DataEntry(string str)
+{
+    Console.WriteLine(str);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+  int Sum(int number)
+  {    
+    int counter = Convert.ToString(number).Length;
+    int ost = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      ost = number - number % 10;
+      result = result + (number - ost);
+      number = number / 10;
+    }
+   return result;
+  }
+int number = DataEntry("Введите число: ");
+int sum = Sum(number);
+Console.WriteLine("Сумма цифр в числе: " + sum);
---
+++ Zadacha_29/Program.cs
@@ -0,0 +1,32 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+
+    int number = 8;    
+
+int[] FillArray(int[] array)
+{
+    int lenght = array.Length;
+    
+        for (int i = 0; i < lenght; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int index;
+
+    for (index = 0; index < count; index++)
+    {
+        Console.WriteLine(col[index] + " ");
+    }
+}
+int[] array = new int[number];
+array = FillArray(array);
+PrintArray(array);
